e1bfc7dea84c5233a8ff2881497e5ba3deef0598
natiorlova-hue <nati.orlova@ihomemarbella.com>
2026-03-25 13:53:49 +0200

    fix: pwa debug (#64)

--- next.config.ts
+++ next.config.ts
@@ -10,8 +10,8 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
 
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
+  cacheOnFrontEndNav: false,
+  aggressiveFrontEndNavCaching: false,
   reloadOnOnline: true,
 });
 
